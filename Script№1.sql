5ed94908e5a8cc04e0425015bd0a33b4ec7435ba
Andrey <75995827+AndiShal95@users.noreply.github.com>
2022-10-18 08:50:15 +0300

    Update Script№1.sql

--- Script№1.sql
+++ Script№1.sql
@@ -1,13 +1,15 @@
 -- Домашняя работа №1
 
 --01
-SELECT DISTINCT status_id qty
-	, src_office_id
-	, dictGet('dictionary.BranchOffice', 'office_name', src_office_id) office_name
-	, position_id position
+SELECT src_office_id   - ошибка  SQL Error [1002]
+	, uniq(item_id) qty
+	, dictGet('dictionary.BranchOffice','office_name', src_office_id) src_office_name
+	, position_id
 FROM history.OrderDetails
-WHERE dt >= now() - INTERVAL 2 DAY
-ORDER BY position DESC
+WHERE dt >= toStartOfDay(now()) - INTERVAL 2 DAY
+	AND status_id = 18
+GROUP BY position_id
+ORDER BY qty DESC
 LIMIT 20
 
 
