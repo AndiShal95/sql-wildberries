7daabee178d62dea14965bbc60149b2c70a78a92
Andrey <75995827+AndiShal95@users.noreply.github.com>
2022-10-20 12:55:32 +0300

    Update Homework-№2-part1.sql

--- Homework-№2-part1.sql
+++ Homework-№2-part1.sql
@@ -1,6 +1,28 @@
---Домашняя работа №2 user 106
+-- 01 Создать таблицу со своим номером tmp.table10_ (Например, tmp.table115)
+-- Движок MergeTree.
+-- Партиционирование: для номеров 104-106 по 1 неделе
+-- Сортировка position_id: для номеров 104-106 по src_office_id
+-- гранулярность индекса 8192
+-- Колонки:
+--     `dt`            DateTime
+--     `position_id`   UInt64
+--     `item_id`       UInt64
+--     `src_office_id` UInt32
+--     `dst_office_id` UInt32
+CREATE TABLE tmp.table106
+(
+    `dt` DateTime,
+    `position_id` UInt64,
+    `item_id` UInt64,
+    `src_office_id` UInt32,
+    `dst_office_id` UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMMDD(dt)
+ORDER BY position_id
+TTL dt + toIntervalDay(7)
+SETTINGS index_granularity = 8192
 
--- задание 01
+-- 02 Добавить материализованную колонку dt_date с типом Date, которая будет считать текущую дату от колонки dt.
 
 
--- задание 02
